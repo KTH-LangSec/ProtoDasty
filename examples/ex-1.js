3837be8b964efd87019a623f92aad9e7d0bcc614
Mateus Marinheiro <72341095+MateusMarinheiro@users.noreply.github.com>
2025-02-18 13:55:01 +0100

    feat: example files for testing

--- examples/ex-1.js
+++ examples/ex-1.js
@@ -1,9 +1,13 @@
 function entryPoint(arg1, arg2, arg3) {
   const obj = {};
-  const p = obj[arg1];
-  var p2 = p[arg2]
-  p2 = arg3;
-  return p;
+  const pollution = arg3;
+  obj[arg1][arg2][pollution] = arg3;
+
+  const obj1 = {};
+  const p = obj1[arg1];
+  p[arg2] = arg3;
+
+  return;
 }
 
-entryPoint("__proto__", "x", 42);
+entryPoint("constructor", "prototype", "42");
