a8a288b361fedb2fde0df7a6a60b84bb73da8a5d
MateusMarinheiro <72341095+MateusMarinheiro@users.noreply.github.com>
2025-03-25 10:17:46 +0100

    fix: lodash test syntax

--- prototype-pollution/lodash_4.17.15/lodash.test.js
+++ prototype-pollution/lodash_4.17.15/lodash.test.js
@@ -1,15 +1,12 @@
 //https://security.snyk.io/vuln/SNYK-JS-LODASH-608086
- 
   const lod = require("lodash");
   let obj = {};
-   
-  lod.set(obj, "__proto__[polluted]", "yes");
-   
-  delete obj.__proto__.polluted; // deletes the property set by this pollution
-});
 
- 
+  lod.set(obj, "__proto__[polluted]", "yes");
+
+  delete obj.__proto__.polluted; // deletes the property set by this pollution
+
+
   const lod = require("lodash");
-   
+
   lod.setWith({}, "__proto__[polluted]", "yes");
-// End of file
