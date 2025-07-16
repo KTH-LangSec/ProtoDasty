2cae3584320b754387b1afc6dfa0fc714c25ee44
Mateus Marinheiro <72341095+MateusMarinheiro@users.noreply.github.com>
2025-07-16 19:11:09 +0100

    feat: new benchmark

--- benchmark/graphs.py
+++ benchmark/graphs.py
@@ -8,8 +8,13 @@ def tool_exec(pkg, y):
     colors = ['#3D9255', '#F45050', '#FFC107', '#512DA8']
     width = 0.65
 
-    y1 = [0, 0, y[1], y[2]]
-    y2 = [y[0], y[1], y[2] - y[1], y[3] - y[2]]
+    if (y[2] > y[1]):
+        y1 = [0, 0, y[1], y[2]]
+        y2 = [y[0], y[1], y[2] - y[1], y[3] - y[2]]
+    else:
+        y1 = [0, 0, 0, y[2]]
+        y2 = [y[0], y[1], y[2], y[3] - y[2]]
+        
 
     plt.title('Performance Overhead - ' + pkg)
     plt.ylabel('Execution Time (in seconds)')
@@ -26,6 +31,7 @@ def tool_exec(pkg, y):
         plt.text(bar.get_x() + bar.get_width() / 2, y[bar_idx] + height_diff, str(y[bar_idx]) + 's', ha='center', va='bottom')
 
     # plt.show()
+    print("Saving", pkg)
     plt.savefig('overhead-' + pkg + '.png', dpi=600)
     # pdf = PdfPages('overhead-' + pkg + '.pdf')
     # pdf.savefig()
@@ -76,31 +82,25 @@ def perf_ratio(data):
 
 
 def main():
-    small = [0.09, 1.82, 2.3, 2.35]
+    small = [0.035, 1.504, 2.327, 2.888]
     tool_exec(
         'small',
         small
     )
 
-    express = [2.39, 23.2, 43.65, 165.29]
+    express = [22.97, 244.05, 323.04, 805.84]
     tool_exec(
-        'express',
+        'uglify-js',
         express
     )
 
-    gm = [0.45, 3.26, 4.36, 5.37]
+    gm = [0.473, 3.8, 5.536, 7.622]
     tool_exec(
         'gm',
         gm
     )
 
-    gm_2 = [8.09, 14.36, 16.47, 19.28]
-    # tool_exec(
-    #     'gm-2',
-    #     gm_2
-    # )
-
-    fs_extra = [5.91, 11.89, 16.33, 25.1]
+    fs_extra = [5.721, 13.549, 18.788, 36.529]
     tool_exec(
         'fs-extra',
         fs_extra
@@ -128,45 +128,39 @@ def main():
         d_avg.append((d_small[i] + d_gm[i] + d_fs[i] + d_express[i]) / 4)
     print(d_avg)
 
-    the_tool_small = [2.32, 5.37]
-    augur_small = [3.42, 23.21]
-    the_tool_big = [25.1, 165.29]
-    augur_big = [300, 300]
+    # the_tool_small = [2.32, 5.37]
+    # augur_small = [3.42, 23.21]
+    # the_tool_big = [25.1, 165.29]
+    # augur_big = [300, 300]
 
-    overhead_small = ((augur_small[0] / the_tool_small[0]) + (augur_small[1] / the_tool_small[0])) / 2
-    overhead_big = ((augur_big[0] / the_tool_big[0]) + (augur_big[1] / the_tool_big[0])) / 2
-    overhead_avg = (overhead_big + overhead_small) / 2
-    print(overhead_small)
-    print(overhead_big)
-    print(overhead_avg)
+    # overhead_small = ((augur_small[0] / the_tool_small[0]) + (augur_small[1] / the_tool_small[0])) / 2
+    # overhead_big = ((augur_big[0] / the_tool_big[0]) + (augur_big[1] / the_tool_big[0])) / 2
+    # overhead_avg = (overhead_big + overhead_small) / 2
+    # print(overhead_small)
+    # print(overhead_big)
+    # print(overhead_avg)
 
-    comp(
-        ['small.js', 'gm'],
-        the_tool=the_tool_small,
-        augur=augur_small,
-        bar_labels=[
-            ['2.35s', '5.37s'],
-            ['3.42s', '23.21s']
-        ],
-        filename='comp-small.pdf'
-    )
+    # comp(
+    #     ['small.js', 'gm'],
+    #     the_tool=the_tool_small,
+    #     augur=augur_small,
+    #     bar_labels=[
+    #         ['2.35s', '5.37s'],
+    #         ['3.42s', '23.21s']
+    #     ],
+    #     filename='comp-small.pdf'
+    # )
 
-    comp(
-        ['fs-extra', 'express'],
-        the_tool=the_tool_big,
-        augur=augur_big,
-        bar_labels=[
-            ['25.1s', '165.29s'],
-            ['300s (timeout)', '300s (timeout)']
-        ],
-        filename='comp-big.pdf'
-    )
-
-    # augur
-    # small.js - success
-    # gm - success -
-    # express - timeout - 1 test case
-    # fs-extra - timeout - 37 - 34 timed out
+    # comp(
+    #     ['fs-extra', 'express'],
+    #     the_tool=the_tool_big,
+    #     augur=augur_big,
+    #     bar_labels=[
+    #         ['25.1s', '165.29s'],
+    #         ['300s (timeout)', '300s (timeout)']
+    #     ],
+    #     filename='comp-big.pdf'
+    # )
 
 
 if __name__ == '__main__':
